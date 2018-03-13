948d886cd471d73425de26087b7dbb4282bba572
Chiba Tairi <tairi0717@gmail.com>
2018-03-13 19:42:56 +0900

    複数回線の切り替えに対応した

--- app/src/main/java/pakutoma/miowidget/activity/WidgetSettingsActivity.kt
+++ app/src/main/java/pakutoma/miowidget/activity/WidgetSettingsActivity.kt
@@ -6,7 +6,12 @@ import android.preference.PreferenceCategory
 import android.preference.PreferenceFragment
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import org.jetbrains.anko.db.classParser
+import org.jetbrains.anko.db.parseList
+import org.jetbrains.anko.db.select
 import pakutoma.miowidget.R
+import pakutoma.miowidget.utility.CouponInfoFromDb
+import pakutoma.miowidget.utility.database
 
 /**
  * Created by PAKUTOMA on 2018/03/13.
@@ -27,14 +32,29 @@ class WidgetSettingsActivity : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             addPreferencesFromResource(R.xml.activity_widget_settings)
-            (preferenceScreen.findPreference("show_remains_plans") as? PreferenceCategory)?.addPreference(CheckBoxPreference(this.activity).apply {
-                title = "hddXXXXXXXX (Light Start)"
-                key="add"
-            })
-            (preferenceScreen.findPreference("switch_lines") as? PreferenceCategory)?.addPreference(CheckBoxPreference(this.activity).apply {
-                title = "080XXXXXXXX (hddXXXXXXXX)"
-                key="add"
-            })
+            activity.database.use {
+                val couponInfo = select("coupon_info",
+                        "service_code",
+                        "plan_name",
+                        "number",
+                        "regulation")
+                        .exec { parseList(classParser<CouponInfoFromDb>()) }
+                val showRemainsPlans = preferenceScreen.findPreference("show_remains_plans")!! as PreferenceCategory
+                couponInfo.map { it.plan to it.serviceCode }.distinct().forEach {
+                    showRemainsPlans.addPreference(CheckBoxPreference(activity).apply {
+                        title = it.first
+                        key = it.second
+                    })
+                }
+                val switchLines = preferenceScreen.findPreference("switch_lines")!! as PreferenceCategory
+                couponInfo.map { (it.line to it.regulation) to it.number }.distinct().forEach {
+                    switchLines.addPreference(CheckBoxPreference(activity).apply {
+                        title = it.first.first
+                        summary = if(it.first.second) resources.getText(R.string.regulation).toString() else ""
+                        key = it.second
+                    })
+                }
+            }
         }
     }
 
--- app/src/main/java/pakutoma/miowidget/service/CouponFunctions.kt
+++ app/src/main/java/pakutoma/miowidget/service/CouponFunctions.kt
@@ -1,13 +1,19 @@
 package pakutoma.miowidget.service
 
 import android.content.Context
+import android.database.sqlite.SQLiteException
 import android.widget.Toast
 import com.github.kittinunf.fuel.core.HttpException
 import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.withContext
+import org.jetbrains.anko.db.*
+import org.jetbrains.anko.defaultSharedPreferences
 import pakutoma.miowidget.R
 import pakutoma.miowidget.exception.NotFoundValidTokenException
 import pakutoma.miowidget.utility.CouponAPI
+import pakutoma.miowidget.utility.CouponInfo
+import pakutoma.miowidget.utility.database
 import pakutoma.miowidget.widget.updateSwitchStatus
 
 /**
@@ -15,11 +21,11 @@ import pakutoma.miowidget.widget.updateSwitchStatus
  * Functions to use CouponAPI.
  */
 suspend fun fetchRemains(context: Context) {
-    accessCoupon(context,false)
+    accessCoupon(context, false)
 }
 
 suspend fun switchCoupon(context: Context) {
-    accessCoupon(context,true)
+    accessCoupon(context, true)
 }
 
 private suspend fun accessCoupon(context: Context, change: Boolean) {
@@ -32,7 +38,7 @@ private suspend fun accessCoupon(context: Context, change: Boolean) {
             throw NotFoundValidTokenException("Not found token in preference.")
         }
         val coupon = CouponAPI(developerID, accessToken)
-        val (isCouponEnabled, remains, serviceCodeList) = fetchCouponInfo(coupon, change)
+        val (isCouponEnabled, remains, serviceCodeList) = fetchCouponInfo(context, coupon, change)
         if (change) {
             coupon.changeCouponUse(isCouponEnabled, serviceCodeList)
             editor.putBoolean("is_coupon_enabled", isCouponEnabled)
@@ -50,22 +56,77 @@ private suspend fun accessCoupon(context: Context, change: Boolean) {
     editor.apply()
 }
 
-private suspend fun showResult(context: Context, change: Boolean, hasToken: Boolean, couldGet: Boolean = false, remains: Int = -1, isCouponEnabled: Boolean = false) {
-    withContext(UI) {
-        if (change) {
-            sendToast(context, hasToken, couldGet, isCouponEnabled)
+private suspend fun showResult(context: Context, change: Boolean, hasToken: Boolean, couldGet: Boolean = false, remains: Int = -1, isCouponEnabled: Boolean = false) = withContext(UI) {
+    if (change) {
+        sendToast(context, hasToken, couldGet, isCouponEnabled)
+    }
+    updateSwitchStatus(context, hasToken, couldGet, remains, isCouponEnabled)
+}
+
+private suspend fun fetchCouponInfo(context: Context, coupon: CouponAPI, change: Boolean): Triple<Boolean, Int, List<String>> {
+    val couponInfo = coupon.fetchCouponInfo()
+    launch { updateDb(context, couponInfo) }
+    val (enablePlans, enableLines) = getEnablePlansAndLines(context, couponInfo)
+    val isCouponEnabledNow = if (enableLines.isEmpty()) {
+        couponInfo.planInfoList[0].lineInfoList[0].couponUse
+    } else {
+        couponInfo.planInfoList.flatMap { it.lineInfoList.filter { it.number == enableLines.first() }.map { it.couponUse } }.first()
+    }
+    val isCouponEnabled = if (!change) isCouponEnabledNow else !isCouponEnabledNow
+    val remains = if (enablePlans.isEmpty()) {
+        couponInfo.planInfoList.sumBy { it.remains }
+    } else {
+        couponInfo.planInfoList.filter { x -> enablePlans.any { it == x.serviceCode } }.sumBy { it.remains }
+    }
+    val serviceCodeList = if (!change) emptyList<String>() else {
+        if (enableLines.isEmpty()) {
+            couponInfo.planInfoList.flatMap { it.lineInfoList.map { it.serviceCode } }
+        } else {
+            couponInfo.planInfoList.flatMap { it.lineInfoList.filter { x -> enableLines.any { it == x.number } }.map { it.serviceCode } }
         }
-        updateSwitchStatus(context, hasToken, couldGet, remains, isCouponEnabled)
+    }
+    return Triple(isCouponEnabled, remains, serviceCodeList)
+}
+
+private fun updateDb(context: Context, couponInfo: CouponInfo) {
+    context.database.use {
+        val dbLineList = select("coupon_info", "number")
+                .distinct()
+                .exec { parseList(StringParser) }
+        val lineInsert = couponInfo.planInfoList.flatMap { it.lineInfoList.map { it.number } }.subtract(dbLineList)
+        val lineDelete = dbLineList.subtract(couponInfo.planInfoList.flatMap { it.lineInfoList.map { it.number } })
+        val lineUpdate = couponInfo.planInfoList.flatMap { it.lineInfoList.map { it.number } }.intersect(dbLineList)
+        for (line in lineInsert) {
+            val planInfo = couponInfo.planInfoList.find { it.lineInfoList.any { it.number == line } }!!
+            val lineInfo = couponInfo.planInfoList.flatMap { it.lineInfoList.filter { it.number == line } }.first()
+            insert("coupon_info",
+                    "service_code" to planInfo.serviceCode,
+                    "plan_name" to planInfo.plan,
+                    "number" to lineInfo.number,
+                    "regulation" to lineInfo.regulation
+            )
+        }
+        for (line in lineDelete) {
+            delete("coupon_info",
+                    "number = {number}",
+                    "number" to line)
+        }
+        for (line in lineUpdate) {
+            val planInfo = couponInfo.planInfoList.find { it.lineInfoList.any { it.number == line } }!!
+            val lineInfo = couponInfo.planInfoList.flatMap { it.lineInfoList.filter { it.number == line } }.first()
+            update("coupon_info",
+                    "service_code" to planInfo.serviceCode,
+                    "plan_name" to planInfo.plan,
+                    "regulation" to lineInfo.regulation)
+        }
     }
 }
 
-private suspend fun fetchCouponInfo(coupon: CouponAPI, change: Boolean): Triple<Boolean, Int, List<String>> {
-    val couponInfo = coupon.fetchCouponInfo()
-    val nowCouponValue = couponInfo.planInfoList[0].lineInfoList[0].couponUse
-    val isCouponEnabled = if (!change) nowCouponValue else !nowCouponValue
-    val remains = couponInfo.planInfoList.sumBy { it.remains }
-    val serviceCodeList = if (!change) emptyList<String>() else couponInfo.planInfoList.flatMap { it.lineInfoList.map { it.serviceCode } }
-    return Triple(isCouponEnabled, remains, serviceCodeList)
+private fun getEnablePlansAndLines(context: Context, couponInfo: CouponInfo): Pair<List<String>, List<String>> {
+    val preference = context.defaultSharedPreferences
+    val enablePlans = couponInfo.planInfoList.filter { preference.getBoolean(it.serviceCode, false) }.map { it.serviceCode }
+    val enableLines = couponInfo.planInfoList.flatMap { it.lineInfoList.filter { preference.getBoolean(it.number, false) }.map { it.number } }
+    return Pair(enablePlans, enableLines)
 }
 
 private fun sendToast(context: Context, hasToken: Boolean, couldChange: Boolean = false, isCouponEnabled: Boolean = false) {
